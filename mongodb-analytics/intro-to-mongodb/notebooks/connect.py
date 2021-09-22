1a8feee2e191b595ed2217506478073cdcdf97cd
nitrek <koolestnitesh@gmail.com>
2021-09-23 00:19:55 +0530

    added mongdb connection code

---
+++ mongodb-analytics/intro-to-mongodb/notebooks/connect.py
@@ -0,0 +1,8 @@
+def get_mongo_client():
+    from pymongo import MongoClient
+    from dotenv import dotenv_values
+    config = dotenv_values(".env")
+    # Replace XXXX with your connection URI from the Atlas UI
+    client = MongoClient(config['connection_url'])
+    print(f"Connected to MongoDB Atlas with URI: {config['connection_url']}")
+    return client
