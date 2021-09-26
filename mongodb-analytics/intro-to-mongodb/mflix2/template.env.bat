762452905eb71af54ec6effe77932a48cfbbf466
nitrek <koolestnitesh@gmail.com>
2021-09-27 01:09:27 +0530

    added mflix video streaming service

---
+++ mongodb-analytics/intro-to-mongodb/mflix2/template.env.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=mflix/mflix.py
+set FLASK_DEBUG=false
+set MFLIX_DB_URI="mongodb+srv://username:password@host/myFirstDatabase?retryWrites=true&w=majority" & REM Replace XXXX with your MongoDB Connection URI
